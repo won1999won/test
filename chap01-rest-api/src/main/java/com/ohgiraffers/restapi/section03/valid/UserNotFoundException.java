b23b139bdf9c7c84cb84224331da5c6c4a6c9dc5
won1999won <157452264+won1999won@users.noreply.github.com>
2024-05-17 18:19:07 +0900

    Delete chap01-rest-api directory

--- chap01-rest-api/src/main/java/com/ohgiraffers/restapi/section03/valid/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ohgiraffers.restapi.section03.valid;
-
-public class UserNotFoundException extends Exception {
-    public UserNotFoundException(String no) {
-        super(no);
-    }
-}
